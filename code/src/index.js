e047c2b1998f7c8745449a62b40b84678661e4d7
AleksanderTidemann <aleksandertid@gmail.com>
2022-01-15 10:57:24 +0100

    Hello world

---
+++ code/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./css/App.css";
+import "./css/custom-style.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
